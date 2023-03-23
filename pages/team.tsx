fabe77f5df3f45e8300d70d95679d37d4ab163a9
namle <nam@vietswiss.com>
2023-03-23 16:34:08 +0700

    story slider

--- pages/team.tsx
+++ pages/team.tsx
@@ -3,12 +3,34 @@ import { GetStaticProps } from 'next';
 import { initializeApollo } from '@/utils/apolloClient';
 import { GET_TEAM_SECTION } from '@/graphql/query/teamSection';
 import StorySlider from '@/organisms/slider/story';
-const Team = ({ payload }: any) => {
-  console.log(payload);
+import { TeamSection, Team } from '@/graphql/generated';
+import { Fragment } from 'react';
+
+interface Props {
+  teamSection: {
+    data: {
+      attributes: TeamSection;
+    };
+  };
+  teams: Team;
+}
+const Team = ({ payload }: { payload: Props }) => {
   return (
     <DefaultLayout>
       <div className="min-h-[100vh] px-[20px]">
-        <StorySlider />
+        {payload.teamSection.data.attributes.contents &&
+          payload.teamSection.data.attributes.contents.map(
+            (item: any, key: number) => {
+              switch (item.__typename) {
+                case 'ComponentContentStory':
+                  return (
+                    <Fragment key={key}>
+                      <StorySlider data={item} title={item.title} />
+                    </Fragment>
+                  );
+              }
+            }
+          )}
       </div>
     </DefaultLayout>
   );
