f5b6f929f50b50203928f9cea3f504235e413645
khoa <khoa@vietswiss.com>
2023-03-27 13:32:38 +0700

    feat: finish update layout item for accordion

--- molecules/commons/accordions/index.tsx
+++ molecules/commons/accordions/index.tsx
@@ -3,6 +3,7 @@ import AccordionHeader from './header';
 import AccordionContent from './content';
 import {
   ComponentAccordionAccordion,
+  ComponentBasicButtonLink,
   Maybe,
   UploadFileEntityResponse
 } from '@/graphql/generated';
@@ -11,6 +12,7 @@ import Image from 'next/image';
 import Text from '@/atoms/typo/text';
 import PrimaryButton from '@/atoms/button';
 import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
 const Accordion = ({
   list,
@@ -42,9 +44,27 @@ const Accordion = ({
               )}
               {item && (
                 <AccordionContent index={key} expanded={expanded}>
-                  {item.image &&
-                    renderImage({ image: item.image, layout: `layout-grid` })}
-                  {true && (
+                  {item.image && item.layout != 'Collapse' && (
+                    <>
+                      <div
+                        className={`flex flex-wrap pb-[16px] lg:mx-10 xl:mx-12`}
+                      >
+                        {renderImage({
+                          image: item.image
+                        })}
+                        {item.layout == 'Grid' &&
+                          item.content &&
+                          renderGrid({
+                            content: item.content,
+                            button: { id: '', url: ``, title: `` }
+                          })}
+                        {item.layout == 'Markdown' &&
+                          item.content &&
+                          renderMarkdown({ content: item.content })}
+                      </div>
+                    </>
+                  )}
+                  {item.layout == `Collapse` && (
                     <Collapse
                       content={item.content}
                       className={`max-w-[841px] pb-4 lg:mx-12`}
@@ -62,20 +82,10 @@ const Accordion = ({
 
 export default Accordion;
 
-const renderImage = ({
-  image,
-  layout = 'layout-grid'
-}: {
-  image: UploadFileEntityResponse;
-  layout: 'layout-grid' | 'layout-markdown';
-}) => {
+const renderImage = ({ image }: { image: UploadFileEntityResponse }) => {
   const imageData = image.data?.attributes;
-  const clsGrid = `flex flex-wrap pb-4 lg:mx-12`;
-  const clsMarkdown = `max-w-[841px] pb-4 lg:mx-12`;
   return (
-    <div
-      className={`pb-[16px] ${layout == `layout-grid` ? clsGrid : clsMarkdown}`}
-    >
+    <>
       {imageData && imageData.url && (
         <Image
           src={imageData.url}
@@ -85,20 +95,33 @@ const renderImage = ({
           className={`w-full lg:w-[65%]`}
         />
       )}
+    </>
+  );
+};
 
-      {imageData && layout == 'layout-grid' && (
-        <div className={`w-full lg:w-[35%] lg:px-7`}>
-          <Text
-            size={`bodyTextSmall`}
-            content={`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. `}
-            className={`pb-[25px] lg:max-w-[305px] xl:pb-11`}
-          />
-          <Link href={`/`} title={`mehr erfahren`}>
-            <PrimaryButton title={`mehr erfahren`} />
-          </Link>
-        </div>
+const renderGrid = ({
+  content,
+  button
+}: {
+  content: string;
+  button: ComponentBasicButtonLink;
+}) => {
+  return (
+    <div className={`w-full lg:w-[35%] lg:pl-7`}>
+      <Text
+        size={`bodyTextSmall`}
+        content={content}
+        className={`pb-[25px] lg:max-w-[305px] xl:pb-11`}
+      />
+      {button && button.url && (
+        <Link href={button.url} title={button.title ?? ''}>
+          <PrimaryButton title={button.title ?? ''} />
+        </Link>
       )}
-      {/* {button && button } */}
     </div>
   );
+};
+
+const renderMarkdown = ({ content }: { content: string }) => {
+  return <ReactMarkdown className="custom-markdown">{content}</ReactMarkdown>;
 };
