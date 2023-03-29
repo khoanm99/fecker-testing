afcbf0c69e3b47b85234d2cd133f1d5428fdc6d1
khoa <khoa@vietswiss.com>
2023-03-29 13:35:39 +0700

    fix: refactor code

--- molecules/projectNavigation/NavigationSection.tsx
+++ molecules/projectNavigation/NavigationSection.tsx
@@ -1,6 +1,10 @@
+import NavigationSvg from '@/atoms/svg/navigation';
+import Text from '@/atoms/typo/text';
 import { ProjectRelationResponseCollection } from '@/graphql/generated';
+import clsx from 'clsx';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import TextWithNavigate from './TextWithNavigate';
+import { twMerge } from 'tailwind-merge';
 
 const NavigatorSection = ({
   listProject,
@@ -33,17 +37,14 @@ const NavigatorSection = ({
     <>
       {listProject && listNavigateIndex && (
         <div className={`justify-between md:flex md:flex-row-reverse`}>
-          {listNavigateIndex.map((projectIndex, key) => {
-            return (
-              <TextWithNavigate
-                url={`/projekte/${listProject.data[projectIndex].attributes?.slug}`}
-                key={key}
-                title={listProject.data[projectIndex].attributes?.name}
-                navigate={key == 0 ? 'next' : 'prev'}
-                className={key == 0 ? 'mb-3 md:mb-0' : ''}
-              />
-            );
-          })}
+          {listNavigateIndex.map((projectIndex, key) =>
+            renderTextWithNavigate({
+              url: `/projekte/${listProject.data[projectIndex].attributes?.slug}`,
+              title: listProject?.data[projectIndex]?.attributes?.name,
+              navigate: key == 0 ? 'next' : 'prev',
+              className: key == 0 ? 'mb-3 md:mb-0' : ''
+            })
+          )}
         </div>
       )}
     </>
@@ -51,3 +52,66 @@ const NavigatorSection = ({
 };
 
 export default NavigatorSection;
+
+interface Props {
+  type?: 'link';
+  cls?: string;
+  className?: string;
+  title?: string;
+  url: string;
+  navigate?: 'next' | 'prev';
+}
+
+const renderTextWithNavigate = ({
+  type = 'link',
+  className = '',
+  title = '',
+  url,
+  navigate = 'next'
+}: Props) => {
+  switch (type) {
+    case 'link':
+      return (
+        <>
+          <Link
+            href={url ?? '#'}
+            title={title}
+            className={twMerge(
+              clsx(
+                `flex items-center justify-start gap-5 transition-transform duration-300 lg:justify-center`,
+                {
+                  'flex-row-reverse': navigate == 'prev'
+                },
+                className
+              )
+            )}
+          >
+            {title && renderContentWithNavigator({ title, navigate })}
+          </Link>
+        </>
+      );
+  }
+};
+
+const renderContentWithNavigator = ({
+  title,
+  navigate = 'next'
+}: {
+  title: string;
+  navigate: 'next' | 'prev';
+}) => {
+  return (
+    <>
+      <Text
+        content={title}
+        className={`w-full font-bold lg:font-[16px]`}
+        renderAs={`div`}
+      />
+      <NavigationSvg
+        className={`flex !h-12 !w-12 flex-[0_0_48px] ${
+          navigate == 'next' ? `rotate-180` : ``
+        }`}
+      />
+    </>
+  );
+};
